7ac5710efbf05d1d86eda1b6bcb17051139ec8fa
luigi <luigi.franceschetti@gmail.com>
2015-03-21 11:54:18 +0100

    secondo commit

--- 5bp1/src/ciao.java
+++ 5bp1/src/ciao.java
@@ -4,7 +4,7 @@ public class ciao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-System.out.println("Hello world!");
+System.out.println("Hello world dal 21/3/2015!");
 	}
 
 }
